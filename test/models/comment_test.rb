ce4ab083c69f9bd33275fa0fd0b1df653d306cd1
lujanfernaud <lujanfernaud@gmail.com>
2017-08-14 19:37:00 +0100

    Remove inoperative tests for Comment and Post models

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,12 +1,4 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  def setup
-    @comment = Comment.new(post_id: 1, body: "This is a test comment!")
-  end
-
-  test "should be not valid is the body is not present" do
-    @comment.body = nil
-    assert_not @comment.valid?
-  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,17 +1,4 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  def setup
-    @post = Post.new(user_id: 3, title: "This is a test post", body: "a" * 1000)
-  end
-
-  test "should be not valid if title is missing" do
-    @post.title = ""
-    assert_not @post.valid?
-  end
-
-  test "should be not valid if body is missing" do
-    @post.body = ""
-    assert_not @post.valid?
-  end
 end
